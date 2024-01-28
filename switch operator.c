27c0c1d6e184f5fa15e86bacb909e017719a757c
Md Sabbir Ahmad <152280320+Sabbir943@users.noreply.github.com>
2024-01-28 23:45:04 +0600

    switch

---
+++ switch operator.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Enter any number:");
+  scanf("%d",&num);
+  switch(num)
+  {
+case 1:
+    {
+        printf("one");
+        break;
+    }
+   case 2:
+    {
+        printf("two");
+        break;
+    }
+
+   case 3:
+    {
+        printf("THREE");
+        break;
+    }
+
+    case 4:
+    {
+        printf("four");
+        break;
+    }
+    case 5:
+    {
+        printf("five");
+        break;
+    }
+    default :
+        printf("wrong input");
+
+
+
+
+
+
+
+
+
+  }
+
+}
